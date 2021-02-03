6f923f21b66902b3dba360a3ea18149488d0c402
BostonSlettum22 <816661@mystma.org>
2021-02-03 16:39:51 +0000

    reverse word 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def reversal(w):
+    bazinga = w[len(w)-1]
+
+    for i in range(2,len(w)+1):
+        bazinga = bazinga + w[len(w)-i]
+
+
+    return bazinga   
+
+print(reversal("polaroid"))
